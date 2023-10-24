e5bf71d529c3490afa4e84ae2cdfebdc6db2cf84
rajesh kumar <rajesh.kumar@bchydro.com>
2023-10-24 07:01:49 -0700

    Project structure and api changes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
-import { Elysia } from "elysia";
 import app from "./app";
 
-const elysia = new Elysia();
-
-elysia.use(app);
-elysia.listen(Bun.env.PORT || 3000);
+app.listen(Bun.env.PORT ?? 8080);
 console.log(
-  `Server is running at ${elysia.server?.hostname}:${elysia.server?.port}`
+  `Server is running at ${app.server?.hostname}:${app.server?.port}`
 );
