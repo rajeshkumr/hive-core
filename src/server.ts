08b5bbd93bba6e84f6e8bd11f37446b8cc281b86
rajesh kumar <rajesh.kumar@bchydro.com>
2023-12-05 06:48:10 -0800

    Port number changed

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-app.listen(process?.env?.PORT ?? 8080);
+app.listen(process?.env?.PORT ?? 3000);
 console.log(
   `Server is running at ${app.server?.hostname}:${app.server?.port}`
 );
