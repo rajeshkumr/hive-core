aa5049038fd5f41ce38c537e2b901f0c480a7011
rajesh kumar <rajesh.kumar@bchydro.com>
2024-01-04 20:48:40 -0800

    Env variables print for testing

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,6 @@ app.listen(process?.env?.PORT ?? 3000);
 console.log(
   `Server is running at ${app.server?.hostname}:${app.server?.port}`
 );
+console.log(
+  `ENV variables ${JSON.stringify(process.env)}`
+);
